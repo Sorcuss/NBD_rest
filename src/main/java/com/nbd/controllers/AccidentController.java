25420b856198b7e79075a3b34d1a43feab8479b9
Sorcuss <31709862+Sorcuss@users.noreply.github.com>
2019-11-11 15:11:41 +0100

    change status code and add documentation

--- src/main/java/com/nbd/controllers/AccidentController.java
+++ src/main/java/com/nbd/controllers/AccidentController.java
@@ -2,11 +2,19 @@ package com.nbd.controllers;
 
 import com.nbd.models.Accident;
 import com.nbd.repositories.AccidentRepository;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Repository;
 import org.springframework.web.bind.annotation.*;
 
 import javax.print.attribute.standard.Media;
+import javax.xml.ws.Response;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
@@ -18,34 +26,74 @@ public class AccidentController {
     AccidentRepository accidentRepository;
 
     @GetMapping(value = "/accidents", produces = MediaType.APPLICATION_JSON_VALUE)
+    @ApiOperation(value = "Get all accidents")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "OK")
+    })
     public List<Accident> getAllAccidents(){
         return accidentRepository.findAll();
     }
 
-    @PostMapping(value = "/accident" , consumes = MediaType.APPLICATION_JSON_VALUE)
-    public String addAccidents(@RequestBody Accident accident) {
-        accident.setAccidentIndex(UUID.randomUUID().toString());
+    @ApiOperation(value = "Create accident")
+    @PostMapping(value = "/accident" , consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    @ResponseStatus(HttpStatus.CREATED)
+    @ResponseBody
+    @ApiResponses(value = {
+            @ApiResponse(code = 201, message = "Created"),
+            @ApiResponse(code = 400, message = "Bad request")
+    })
+    public ResponseEntity<Void> addAccidents(@RequestBody Accident accident) {
+        if(accident.getAccidentIndex() == null || accident.getAccidentIndex().equals(""))
+            accident.setAccidentIndex(UUID.randomUUID().toString());
         Accident addedAccident = accidentRepository.save(accident);
-        return addedAccident != null ? "success" : "error";
+        if(addedAccident == null){
+            return new ResponseEntity<Void>(HttpStatus.BAD_REQUEST);
+        }
+        return new ResponseEntity<Void>(HttpStatus.CREATED);
     }
 
-    @GetMapping("/accident/{id}")
-    public Accident getAccident(@PathVariable String id){
+
+
+    @ApiOperation(value = "Get accident")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "ok"),
+            @ApiResponse(code = 404, message = "Not found")
+    })
+    @GetMapping(value = "/accident/{id}")
+    @ResponseBody
+    public ResponseEntity<Accident> getAccident(@PathVariable String id){
         Optional<Accident> accident = accidentRepository.findById(id);
-        return accident.orElse(null);
+        return accident.map(value -> new ResponseEntity<>(value, HttpStatus.OK)).orElseGet(() -> new ResponseEntity<>(HttpStatus.NOT_FOUND));
     }
 
+
+    @ApiOperation(value = "Delete accident")
+    @ApiResponses(value = {
+            @ApiResponse(code = 204, message = "no content"),
+            @ApiResponse(code = 404, message = "no found")
+    })
     @DeleteMapping(value = "/accident/{id}")
-    public String deleteAccident(@PathVariable String id){
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @ResponseBody
+    public ResponseEntity<Void> deleteAccident(@PathVariable String id){
         boolean existFlag = accidentRepository.existsById(id);
         if(existFlag){
             accidentRepository.deleteById(id);
+            return new ResponseEntity<Void>(HttpStatus.NO_CONTENT);
         }
-        return existFlag ? "success" : "accident does not exist";
+        return new ResponseEntity<Void>(HttpStatus.NOT_FOUND);
     }
 
-    @PutMapping(value = "/accident/{id}", consumes = MediaType.APPLICATION_JSON_VALUE)
-    public String updateAccident(@PathVariable String id, @RequestBody Accident accident){
+
+    @ApiOperation(value = "Update accident")
+    @ApiResponses(value = {
+        @ApiResponse(code = 201, message = "created"),
+            @ApiResponse(code = 404, message = "not found")
+            })
+    @PutMapping(value = "/accident/{id}", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    @ResponseStatus(HttpStatus.CREATED)
+    @ResponseBody
+    public ResponseEntity<Void> updateAccident(@PathVariable String id, @RequestBody Accident accident){
         Optional<Accident> accidentToUpdate = accidentRepository.findById(id);
         if(accidentToUpdate.isPresent()) {
             Accident updatedAccident = accidentToUpdate.get();
@@ -60,13 +108,15 @@ public class AccidentController {
             updatedAccident.setRoadType(accident.getRoadType());
             updatedAccident.setSpeedLimit(accident.getSpeedLimit());
             updatedAccident.setLightConditions(accident.getLightConditions());
+            updatedAccident.setRoadSurfaceConditions(accident.getRoadSurfaceConditions());
+            updatedAccident.setLocalAuthority(accident.getLocalAuthority());
             updatedAccident.setWeatherConditions(accident.getWeatherConditions());
             updatedAccident.setRoadType(accident.getRoadType());
             updatedAccident.setUrbanOrRuralArea(accident.getUrbanOrRuralArea());
             updatedAccident.setDidPoliceOfficerAttendSceneOfAccident(accident.isDidPoliceOfficerAttendSceneOfAccident());
             accidentRepository.save(updatedAccident);
-            return "success";
+            return new ResponseEntity<Void>(HttpStatus.CREATED);
         }
-        return "accident does not exist";
+        return new ResponseEntity<Void>(HttpStatus.NOT_FOUND);
     }
 }
